71a4b1a4e7d3cfffeefd9a2cc0efc7a0dfd85326
AyeshaBB <Ayeshabb.xworkz@gmail.com>
2021-10-27 11:12:14 +0530

    Method Concept

---
+++ DivisionOfTwoNumbers.java
@@ -0,0 +1,19 @@
+class DivisionOfTwoNumbers{
+
+	public static void main(String a[]){
+
+		System.out.println("main method started");
+
+		division(11,10);
+		division(38,40);
+		division(25,15);		
+
+		System.out.println("main method ended");
+	}
+
+	public static void division(int m, int n){
+
+		System.out.println(m/n);
+	}
+
+}
---
+++ MultiplicationOfTwoNumbers.java
@@ -0,0 +1,19 @@
+class MultiplicationOfTwoNumbers{
+
+	public static void main(String a[]){
+
+		System.out.println("main method started");
+
+		multiplication(11,10);
+		multiplication(38,40);
+		multiplication(25,15);		
+
+		System.out.println("main method ended");
+	}
+
+	public static void multiplication(int x, int y){
+
+		System.out.println(x*y);
+	}
+
+}
